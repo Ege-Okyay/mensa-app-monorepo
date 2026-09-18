d26b6cdfebd04f1307b506b7d0d0ab9e1b337d51
Ege-Okyay <egeokyay0@gmail.com>
2026-09-18 10:45:42 +0200

    big commit

--- cron-jobs/src/config.ts
+++ cron-jobs/src/config.ts
@@ -5,7 +5,27 @@ export interface CronJob {
 
 export const CRON_JOBS: CronJob[] = [
   {
+    cron: '0,20,40 11 * * *',
+    workflows: ['scrape.yml']
+  },
+  {
+    cron: '0,20 12 * * *',
+    workflows: ['scrape.yml'],
+  },
+  {
+    cron: '30,50 17 * * *',
+    workflows: ['scrape.yml'],
+  },
+  {
+    cron: '10 18 * * *',
+    workflows: ['scrape.yml'],
+  },
+  {
     cron: '0 15 * * *',
-    workflows: ['test-dispatch.yml']
+    workflows: ['clear-menus.yml']
+  },
+  {
+    cron: '0 22 * * *',
+    workflows: ['clear-menus.yml']
   }
 ];
