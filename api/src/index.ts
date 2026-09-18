b9291d0520030e8139c844831e75cb40c60e32e1
Ege-Okyay <egeokyay0@gmail.com>
2026-09-18 14:38:38 +0200

    fix: updated local config with the remote values

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,2 @@
 import app from './app';
-
-// console.log('---Active Routes---');
-
-// app.routes.forEach((route) => {
-//   console.log(`${route.method.padEnd(7)} ${route.path}`);
-// });
-
-// console.log('-------------------\n');
-
 export default app;
